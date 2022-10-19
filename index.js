2d14e2e9f64db270883134051d37bde05710d368
Auma <aumizy01@gmail.com>
2022-10-19 22:52:33 +0100

    up 2.0.1

--- index.js
+++ index.js
@@ -1,56 +1,60 @@
-function getPosition () {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            h1.innerHTML = "Location: Longitude " + position.coords.longitude + "\n" + 
-            "Latitude: " + position.coords.latitude
-            console.log(position);
-        })
-    } else {
-        console.log("api not supported");
-    }
-
+function getPosition() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      h1.innerHTML =
+        "Location: Longitude " +
+        position.coords.longitude +
+        "\n" +
+        "Latitude: " +
+        position.coords.latitude;
+      console.log(position);
+    });
+  } else {
+    console.log("api not supported");
+  }
 }
 
-const h1 = document.querySelector('h1')
-const btn = document.querySelector('button')
-const html = document.querySelector('html')
-const attBtn = document.querySelector('#att-btn')
-btn.addEventListener('click', getPosition);
-attBtn.addEventListener('click', markAttendance);
+const h1 = document.querySelector("h1");
+const btn = document.querySelector("button");
+const html = document.querySelector("html");
+const attBtn = document.querySelector("#att-btn");
+btn.addEventListener("click", getPosition);
+attBtn.addEventListener("click", markAttendance);
 window.onload = getUserPosition;
-const defaultLatitude = 9.05535;
-const defaultLongitude = 7.48496;
+const defaultLatitude = 9.0547;
+const defaultLongitude = 7.485;
 
 let userPosition;
 let positionDetail;
 
-function getUserPosition () {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(chek)
-    }
+function getUserPosition() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(chek);
+  }
 }
 
-function chek (position) {
-    positionDetail = position
-    userPosition = {
-        latitude : position.coords.latitude,
-        longitude : position.coords.longitude
-    }
-    // h1.innerHTML = "latitude: " + userPosition.latitude + "longitude: " + userPosition.longitude;
-    console.log(userPosition);
-    console.log(positionDetail);
-    return userPosition
-    // userPosition = getPosition();
+function chek(position) {
+  positionDetail = position;
+  userPosition = {
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude,
+  };
+  // h1.innerHTML = "latitude: " + userPosition.latitude + "longitude: " + userPosition.longitude;
+  console.log(userPosition);
+  console.log(positionDetail);
+  return userPosition;
+  // userPosition = getPosition();
 }
 
-function markAttendance () {
-    if ((userPosition.latitude <= 7.48600) && (userPosition.longitude >= 7.48400) && (userPosition.longitude <= 9.05600) && (userPosition.longitude >= 9.05300) ) {
-        window.location.replace('wrongloc.html');
-    } else {
-        window.location.replace('takeatt.html')
-    }
+function markAttendance() {
+  if (
+    userPosition.latitude <= 7.4849 &&
+    userPosition.longitude >= 7.4851 &&
+    userPosition.longitude <= 9.0546 &&
+    userPosition.longitude >= 9.0548
+  ) {
+    window.location.replace("wrongloc.html");
+  } else {
+    window.location.replace("takeatt.html");
+  }
 }
-
-
-
-
