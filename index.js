17587eb4c24925208376692e579f84304035d6eb
Ikwumonu Emmanuel <IT0057@CHQ.NNPCGroup.local>
2022-10-20 16:55:16 +0100

    rewrote the whole js file with all functionality in the take att btn

--- index.js
+++ index.js
@@ -1,69 +1,48 @@
-function getPosition() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      h1.innerHTML =
-        "Location: Longitude " +
-        position.coords.longitude.toFixed(6) +
-        "\n" +
-        "Latitude: " +
-        position.coords.latitude.toFixed(6);
-      console.log(position);
-    });
-  } else {
-    console.log("api not supported");
-  }
-}
-
 const h1 = document.querySelector("h1");
 const btn = document.querySelector("button");
 const html = document.querySelector("html");
 const attBtn = document.querySelector("#att-btn");
 btn.addEventListener("click", getPosition);
 attBtn.addEventListener("click", markAttendance);
-//window.onload = getUserPosition;
-// const defaultLatitude = 9.054701;
-// const defaultLongitude = 7.485451;
 
 let userPosition;
 let positionDetail;
 let mainLat;
 let mainLong;
 
-function getUserPosition() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(chek);
-  }
+function getPosition () {
+    if (navigator.geolocation) {
+        let latholder;
+        let longholder;
+        navigator.geolocation.getCurrentPosition((position) => {
+            latholder = Number(position.coords.latitude.toFixed(6))
+            longholder = Number(position.coords.longitude.toFixed(6))
+            h1.innerHTML = "longitude: "+ latholder + " latitude: " + longholder;
+        })
+    } else {
+        console.log("API not working");
+    }
 }
 
-function chek(position) {
-  positionDetail = position;
-  userPosition = {
-    latitude: position.coords.latitude,
-    longitude: position.coords.longitude,
-  };
-  // h1.innerHTML = "latitude: " + userPosition.latitude + "longitude: " + userPosition.longitude;
-  let convLatHolder;
-  let convLongHolder;
-  convLatHolder = userPosition.latitude.toFixed(6);
-  convLongHolder = userPosition.longitude.toFixed(6);
-  mainLat = Number(convLatHolder);
-  mainLong = Number(convLongHolder);
-  
-  console.log(userPosition);
-  console.log(mainLat, mainLong);
-  return userPosition;
-  // userPosition = getPosition();
-}
+function markAttendance () {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            positionDetail = position;
+            mainLat = Number(position.coords.latitude.toFixed(6))
+            mainLong = Number(position.coords.longitude.toFixed(6))
 
-function markAttendance() {
-  getUserPosition();
-  if (
-    (mainLat >= 9.053200 && mainLat <= 9.055800) &&
-    (mainLong >= 7.484100 && mainLong <= 7.487200)
-  ) {
-    window.location.replace("takeatt.html");
-  } else {
-    // window.location.replace("takeatt.html");
-    window.location.replace("wrongloc.html");
-  }
+            console.log(userPosition);
+            console.log(mainLat, mainLong);
+            return userPosition;
+        })
+    }
+
+    if (
+        (mainLat >= 9.053200 && mainLat <= 9.055800) &&
+        (mainLong >= 7.484100 && mainLong <= 7.487200)
+    ) {
+        window.location.replace("takeatt.html");
+    } else {
+        window.location.replace("wrongloc.html")
+    }
 }
